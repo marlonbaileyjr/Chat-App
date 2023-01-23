c102a5aea06abac71e51c4072740446e7f7d095b
Marlon Bailey Jr <108299124+Sweatlys@users.noreply.github.com>
2023-01-23 13:35:18 -0500

    first commit

---
+++ renderer/js/change-password.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('reg-form');
+form.addEventListener('submit', registerUser);
+
+async function registerUser(e) {
+    e.preventDefault();
+    const password = document.getElementById('password').value;
+    
+    const result = await fetch('/api/change-password',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            newPassword: password, 
+            token: localStorage.getItem('token')
+        })
+    }).then(res => res.json());
+    
+    if(result.status==='ok'){
+        alert('Password Changed Successfully');
+    }else{
+        alert(result.error);
+    }
+}
