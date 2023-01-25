3e76b48415c39c6f752dcf4f34934ca0744ad062
Marlon Bailey Jr <108299124+Sweatlys@users.noreply.github.com>
2023-01-25 15:54:09 -0500

    css

--- out/ChatApp-win32-x64/resources/app/server.js
+++ out/ChatApp-win32-x64/resources/app/server.js
@@ -15,7 +15,7 @@ const JWT_SECRET='asadcfkjhoiuiblew@nlkvdjnls^kjfbdcls$lifkebcps#jklnbfp;s*jbldv
 
 
 mongoose.set('strictQuery', true)
-mongoose.connect('mongodb://127.0.0.1:27017/chat-app-db'),{
+mongoose.connect('mongodb://admin:Markis55%23@localhost:27017/?authMechanism=default'),{
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
